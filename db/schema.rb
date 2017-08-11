632ace7f9e28def33be7c3fc0841682de535cf84
Gordon <gordon.wallac3@yahoo.com>
2017-08-11 13:32:10 -0600

    added some of the nav bar and styled the rservation page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170811172812) do
 
   # These are extensions that must be enabled in order to support this database
